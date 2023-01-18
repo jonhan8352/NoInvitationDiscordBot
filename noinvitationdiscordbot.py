65a53aa091e03d77344edabd9da902a178007cb3
Jonathan Han <87901116+jonhan8352@users.noreply.github.com>
2023-01-18 19:19:37 +0800

    Update noinvitationdiscordbot.py

--- noinvitationdiscordbot.py
+++ noinvitationdiscordbot.py
@@ -1,12 +1,14 @@
 import discord
+import os
 from discord import Message
+from keep_alive import keep_alive
 
-bot = commands.Bot(command_prefix='!')
+client = discord.Client()
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-
+    
 @bot.event
 async def on_message(message: Message):
     if message.author.bot:
@@ -15,4 +17,5 @@ async def on_message(message: Message):
         if "youtube.com" in message.content or "discord.gg" in message.content or "discordapp.com/invite" in message.content or "twitch.tv" in message.content:
             await message.channel.send("Please do not post links to other YouTube, Discord or Twitch channels here.")
 
-bot.run('YOUR_BOT_TOKEN')
+keep_alive()
+client.run(os.getenv('your_secret_token_key'))
